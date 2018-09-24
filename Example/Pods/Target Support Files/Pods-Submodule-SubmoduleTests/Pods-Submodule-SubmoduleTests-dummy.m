46d8f86adbdcccc1173ab805c70ab72868327859
Kane Cheshire <kane.cheshire@googlemail.com>
2018-09-24 20:30:43 +0100

    pod update

--- Example/Pods/Target Support Files/Pods-Submodule-SubmoduleTests/Pods-Submodule-SubmoduleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Submodule_SubmoduleTests : NSObject
-@end
-@implementation PodsDummy_Pods_Submodule_SubmoduleTests
-@end
--- Example/Pods/Target Support Files/Pods-Submodule/Pods-Submodule-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Submodule : NSObject
-@end
-@implementation PodsDummy_Pods_Submodule
-@end
